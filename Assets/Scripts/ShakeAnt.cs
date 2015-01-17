2d207cfa7c7ae4668d3e9f342378bc01962581b6
Christina716 <cpn716@vt.edu>
2015-01-17 11:55:30 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assembly-CSharp-vs.csproj
    	Assembly-CSharp.csproj
    	Library/InspectorExpandedItems.asset
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/assetDatabase3
    	Library/guidmapper
    	Library/metadata/26/26e68cf741f634d55a3019b124ad76ef
    	Library/metadata/32/3219d602f67924e709a417a0aa7b7e04
    	Library/metadata/4f/4ff7cdffe8ed84bdb99dcbd11cf6776b
    	Library/metadata/60/60b1681b9455f466f9f6f34515cfebca
    	Library/metadata/71/712122a9b79f845dbbbc9e2566ab033c
    	Library/metadata/9b/9b11b982d7d534039a6add4dc7eda60f
    	Library/metadata/c7/c761134b2057843999ed0b24040206b2
    	PennAppsTest-csharp.sln
    	PennAppsTest.sln
    	PennAppsTest.userprefs

---
+++ Assets/Scripts/ShakeAnt.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakeAnt : MonoBehaviour
+{
+	//public GameObject test;
+	public GameObject Ant;
+	public GameObject Ant1;
+	public GameObject Ant2;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Debug.Log(test.transform.position);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Check accelerometer values (inputs)
+		if(Input.acceleration.x==100 || Input.acceleration.y==100 || Input.acceleration.z==100)
+		{
+			//Shake off ant
+			Destroy(GameObject.Find("Ant"));
+		}
+		if(Input.acceleration.x==200 || Input.acceleration.y==200 || Input.acceleration.z==200)
+		{
+			//Shake off ant2
+			Destroy(GameObject.Find("Ant2"));
+		}
+		if(Input.acceleration.x==300 || Input.acceleration.y==300 || Input.acceleration.z==300)
+		{
+			//Shake off ant3
+			Destroy(GameObject.Find("Ant3"));
+		}
+	}
+}
