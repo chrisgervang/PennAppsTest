13b243a82ecd3ea6e8086bafe77d47f05438d69f
Christina716 <cpn716@vt.edu>
2015-01-18 01:35:22 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assembly-CSharp-firstpass-vs.csproj
    	Assembly-CSharp-firstpass.csproj
    	Assembly-CSharp-vs.csproj
    	Assembly-CSharp.csproj
    	PennAppsTest-csharp.sln
    	PennAppsTest.sln

--- Assets/Scripts/ShakeAnt.cs
+++ Assets/Scripts/ShakeAnt.cs
@@ -1,37 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//public static var bundleVersionCode: int;
+
 public class ShakeAnt : MonoBehaviour
 {
 	//public GameObject test;
-	public GameObject Ant;
-	public GameObject Ant1;
-	public GameObject Ant2;
+	private GameObject Ant;
+	private GameObject Ant2;
+	private GameObject Ant3;
+	private GameObject Ant4;
+	private GameObject Ant5;
+	private GameObject Ant6;
+	private GameObject Ant7;
 
 	// Use this for initialization
 	void Start ()
 	{
 		//Debug.Log(test.transform.position);
+		Ant	= GameObject.Find("Ant");
+		Ant2 = GameObject.Find("Ant2");
+		Ant3 = GameObject.Find("Ant3");
+		Ant4 = GameObject.Find("Ant4");
+		Ant5 = GameObject.Find("Ant5");
+		Ant6 = GameObject.Find("Ant6");
+		Ant7 = GameObject.Find("Ant7");
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		// Check accelerometer values (inputs)
-		if(Input.acceleration.x==100 || Input.acceleration.y==100 || Input.acceleration.z==100)
+		if(Input.GetKeyUp(KeyCode.A))
 		{
+			//Debug.Log("KILL ANT ONE");
 			//Shake off ant
-			Destroy(GameObject.Find("Ant"));
+			Destroy(Ant);
 		}
-		if(Input.acceleration.x==200 || Input.acceleration.y==200 || Input.acceleration.z==200)
+		if(Input.GetKeyUp(KeyCode.B))
 		{
-			//Shake off ant2
-			Destroy(GameObject.Find("Ant2"));
+			//Shake off second ant
+			Destroy(Ant2);
 		}
-		if(Input.acceleration.x==300 || Input.acceleration.y==300 || Input.acceleration.z==300)
+		if(Input.GetKeyUp(KeyCode.C))
 		{
-			//Shake off ant3
-			Destroy(GameObject.Find("Ant3"));
+			//Shake off third ant
+			Destroy(Ant3);
+		}
+		if(Input.GetKeyUp(KeyCode.D))
+		{
+			Destroy(Ant4);
+		}
+		if(Input.GetKeyUp(KeyCode.E))
+		{
+			Destroy(Ant5);
+		}
+		if(Input.GetKeyUp(KeyCode.F))
+		{
+			Destroy(Ant6);
+		}
+		if(Input.GetKeyUp(KeyCode.G))
+		{
+			Destroy(Ant7);
 		}
 	}
+
 }
